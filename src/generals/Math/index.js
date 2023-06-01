a151802634c3a965a29af9cb6271553f4aaa87bc
Robert Vásquez <vasquezrober09@gmail.com>
2023-06-01 18:57:11 -0400

    feat: :sparkles: Calculate circle
    calculate area, diameter and circunference of circle

--- src/generals/Math/index.js
+++ src/generals/Math/index.js
@@ -32,4 +32,31 @@ console.log({
   areaTriangulo
 });
 
-console.groupEnd("Triángulo")
+console.groupEnd("Triángulo");
+
+console.group("Circle");
+
+const radioCirculo = 3;
+const diametroCirculo = radioCirculo * 2;
+const PI = 3.1415;
+const circunferencia = diametroCirculo * PI;
+const areaCirculo = (diametroCirculo ** 2) * PI;
+
+console.log({
+  radioCirculo,
+  diametroCirculo,
+  PI,
+  circunferencia,
+  areaCirculo,
+});
+
+function calcularCirculo(radio) {
+  return {
+    radio: radio,
+    diametroCirculo: Math.pow(radio,2),
+    circunferencia: diametroCirculo * Math.PI,
+    areaCirculo: Math.pow(diametroCirculo,2) * Math.PI
+  };
+}
+
+console.groupEnd("Circle");
