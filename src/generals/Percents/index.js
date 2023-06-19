52ac9291bcc9d8ddbc12deecdabd6978a576a666
Robert Vásquez <vasquezrober09@gmail.com>
2023-06-19 16:46:58 -0400

    feat: :sparkles: Coupons
    Use coupons for calculates discounts

--- src/generals/Percents/index.js
+++ src/generals/Percents/index.js
@@ -1,23 +1,32 @@
 const btn = document.querySelector("#calculate");
 const price = document.querySelector("#price");
-const discount = document.querySelector("#discount");
+const coupon = document.querySelector("#coupon");
 const result = document.querySelector("#result");
 
 const calculatePriceWithDiscount = () => {
   const vprice = Number(price.value);
-  const vdiscount = Number(discount.value);
+  const vcoupon = coupon.value;
+  let vdiscount;
 
-  if(!vprice || 0){
-    result.innerText = `Not exists value of price, please insert it`;
-  }else if(!vdiscount || 0){
-    result.innerText = `Not exists value of discount, please insert it`;
-  }else if(vdiscount >= 100) {
-    result.innerText = `Discount not >= 100`;
-  }else{
-    const newPrice = (vprice * (100 - vdiscount)) / 100;
-    result.innerText = `The new price with discount is ${newPrice}$`;
+  if(!vprice || !vcoupon){
+    result.innerText = `Complete the form!`;
+    return;
   }
+
+  switch(vcoupon){
+    case 'Coupon_Batman':
+      vdiscount = 30;
+      break;
+    case 'Coupon_Flash':
+      vdiscount = 15;
+    default:
+      result.innerText = `The "${vcoupon}" is not validated`;
+      return;
+  }
+
+  const newPrice = (vprice * (100 - vdiscount)) / 100;
+  result.innerText = `The new price with discount is ${newPrice}$`;
 
 };
 
-  btn.addEventListener('click', calculatePriceWithDiscount);
+btn.addEventListener('click', calculatePriceWithDiscount);
