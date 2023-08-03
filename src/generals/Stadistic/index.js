06b0eac3821a71ff7fb13932cc225162da2a6830
Robert Vásquez <vasquezrober09@gmail.com>
2023-08-03 11:07:46 -0400

    feat: :sparkles: moda
    Calculate moda function

--- src/generals/Stadistic/index.js
+++ src/generals/Stadistic/index.js
@@ -50,23 +50,51 @@ function calculateAverageWithReduce(list) {
  */
 function orderList(disorderedList) {
   function orderListSort(accumulatedValue, newValue) {
-    // if (accumulatedValue > newValue) {
-    //   return 1;
-    // } else if (accumulatedValue == newValue) {
-    //   return 0;
-    // } else if (accumulatedValue < newValue) {
-    //   return -1;
-    // }
-
     return accumulatedValue - newValue;
-    // return newValue - accumulatedValue;
-    // return 5 - 10 -> -5;
-    // return 5 - 5 -> 0;
-    // return 10 - 5 -> 5;
   }
 
-  // const list = disorderedList.sort(orderListSort);
-  const list = disorderedList.sort((a, b) => a - b);
+  const list = disorderedList.sort(orderListSort);
+  // const list = disorderedList.sort((a, b) => a - b);
 
   return list;
+}
+
+/**
+ * Bidimensional order list function
+ * @return list
+ * @example [ [0,1],  [0,1],  [0,1] ]
+ * */
+function orderListBidimensional(disorderedList, i) {
+  function orderListSort(accumulatedValue, newValue) {
+    return accumulatedValue[i] - newValue[i];
+  }
+
+  const list = disorderedList.sort(orderListSort);
+  return list;
+}
+
+/**
+ * Calculate moda function
+ * @return moda
+ */
+function calculateModa(list) {
+  const listCount = {};
+
+  for (let i = 0; i < list.length; i++) {
+    const element = list[i];
+
+    if (listCount[element]) {
+      listCount[element] += 1;
+    } else {
+      listCount[element] = 1;
+    }
+  }
+
+  const listArray = Object.entries(listCount);
+  const listOrdered = orderListBidimensional(listArray, 1);
+  const listMaxNumber = listOrdered[listOrdered.length - 1];
+  const moda = listMaxNumber[0];
+  // console.log({listCount, listArray, listOrdered, listMaxNumber});
+  // console.log('The moda is: ' + listMaxNumber[0]);
+  return moda;
 }
